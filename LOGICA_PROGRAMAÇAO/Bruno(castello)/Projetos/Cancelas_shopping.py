f230ca43b16c5cd608be6cb1d1572752252a6e1a
Bruno Dias <bdias80549@gmail.com>
2026-05-13 11:27:17 -0300

    Atualizado

---
+++ LOGICA_PROGRAMAÇAO/Bruno(castello)/Projetos/Cancelas_shopping.py
@@ -0,0 +1,109 @@
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+# while True:
+#  print("====Shopping - Cancela====")
+#  escolha0 = print("entrada = e\n saida = s")
+#  input("Digite sua opção:")
+#  if escolha0 == "E".upper() or escolha0 == "e".lower():
+#   print("Liberação de saida")
+#   print("Nós cobramos R$9.00 por hora do veiculo estacionado")
+#   print("Menu de opções\nTicket = T\nCartão = C")
+#   escolha = input("Qual opção você deseja?:")
+#  if escolha0 == "S".upper() or escolha0 == "s".lower():
+#   print("Liberação de saida")
+#   print("Nós cobramos R$9.00 por hora do veiculo estacionado")
+#   print("Menu de opções\nTicket = T\nCartão = C")
+#   escolha = input("Qual opção você deseja?:")
+#   print("___________________________________________________")
+
+#  if escolha == "T   ".upper() or escolha == "t".lower():
+#     print("Você escolheu a opção de Ticket")
+#     placa = input("Digite a placa do seu veiculo:")
+#     modelo = input("Digite o modelo do seu veiculo:")
+#     tempo1 = int(input("Você ficou por quantas horas?"))
+#     total1 = 9*tempo1
+#     print(f"Então sua placa e {placa} e o seu veiculo e um {modelo}, o valor total foi de R$", total1)
+#     print("______________________________________________________________________________________")
+#     print("Pix=P\nCartão=C\nDinheiro=D")
+#     print("______________________________________________________________________________________")
+#     pagamento1 = input("Qual será a forma de pagamento?:")
+#     if pagamento1 == "P".upper() or pagamento1 == "p".lower():
+#        print("QR CODE")
+#        print("Pagamento concluido")
+#        print(f"Notinha:\nO total foi de {total1}")
+#     if pagamento1 == "C".upper() or pagamento1 == "c".lower():
+#        Debito_credito = input("Debito = D\nou\nCredito = C?:")
+#        if Debito_credito == "D".upper() or Debito_credito == "d".lower():
+#           print("Você escolheu debito")
+#           print("Aproxime ou insira para realizar o pagamento")
+#           print("Obrigado volte sempre!!")
+#        if Debito_credito == "C".upper() or Debito_credito == "c".lower():
+#           print("Você escolheu credito")
+#           print("Aproxime ou insira para realizar o pagamento")
+#           print("Obrigado volte sempre!!")
+#     if pagamento1 == "D".upper() or pagamento1 == "d".lower():
+#           print("Obrigado volte sempre!!")       
+#           print("________________________________________________________________________________________")
+#  elif escolha == "C".upper() or escolha == "c".lower():
+#     print("Você escolheu a opção de cartão de acesso ")
+#     placa2 = input("Digite a placa do seu veiculo:")
+#     modelo2 = input("Digite o modelo do seu veiculo:")
+#     tempo2 = int(input("Você ficou por quantas horas?"))
+#     total2 = 9*tempo2
+#     print(f"Então sua placa e {placa2} e o seu veiculo e um {modelo2}, o valor total foi de R$", total2)
+#     print("________________________________________________________________________________________")
+#     print("Pix=P\nCartão=C\nDinheiro=D")
+#     print("________________________________________________________________________________________")
+#     pagamento2 = input("Qual será a forma de pagamento?:")
+#     if pagamento2 == "P".upper() or pagamento2 == "p".lower():
+#        print("QR CODE")
+#        print("Pagamento concluido")
+#        print(f"Notinha:\nO total foi de {total2}")
+#     if pagamento2 == "C".upper() or pagamento2 == "c".lower():
+#        Debito_credito = input("Debito = D\nou\nCredito = C?:")
+#        if Debito_credito == "D".upper() or Debito_credito == "d".lower():
+#           print("Você escolheu debito")
+#           print("Aproxime ou insira para realizar o pagamento")
+#           print("Obrigado volte sempre!!")
+#        if Debito_credito == "C".upper() or Debito_credito == "c".lower():
+#           print("Você escolheu credito")
+#           print("Aproxime ou insira para realizar o pagamento")
+#           print("Obrigado volte sempre!!")
+#           print("________________________________________________________________________________________")
+ 
+ 
+#  else:  
+#     print("Isso não e uma opção\nRedirecionando ao menu")
+#     print("____________________________________________")
+    
+
+
+
+
+
